b1925c2276206f085cdcbf894700b500b9286a4e
github username <github email address>
2019-11-26 16:59:22 -0500

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,12 +6,50 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  state = {
+    sushis: [],
+    iterator: 0,
+    plates: [],
+    wallet: 200
+  }
 
+  componentDidMount() {
+    fetch(API)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          sushis: data
+        })
+      })
+  }
+
+  updatePlates = (sushi) => {
+    if(!this.state.plates.includes(sushi) && 
+    this.state.wallet >= sushi.price){
+    this.setState({
+      plates: [...this.state.plates, sushi],
+      wallet: (this.state.wallet - sushi.price)
+    })
+  }
+  }
+
+  iteratorUpdater = () => {
+    this.setState({
+      iterator: this.state.iterator + 4
+    })
+  }
   render() {
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer 
+          plates={this.state.plates}
+          iteratorUpdater={this.iteratorUpdater} 
+          sushis={this.state.sushis} 
+          iterator={this.state.iterator} 
+          updatePlates={this.updatePlates}
+        />
+        <Table plates={this.state.plates} 
+        wallet={this.state.wallet} />
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,19 +1,21 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
-
+import Sushi from '../components/Sushi'
 const SushiContainer = (props) => {
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
+        {returnFourSushis(props)}
+        <MoreButton iteratorUpdater={props.iteratorUpdater} />
       </div>
     </Fragment>
   )
 }
 
+const returnFourSushis = (props) => {
+  return props.sushis.slice(props.iterator, props.iterator + 4)
+    .map(sushi => {
+      return <Sushi plates={props.plates} key={sushi.id} sushi={sushi} updatePlates={props.updatePlates} />
+    })
+}
 export default SushiContainer
